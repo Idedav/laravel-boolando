763b09a047c55c4e4f28a6d269bdbfb300f5b0f6
Idedav <xdavide.correa@gmail.com>
2023-11-17 16:00:17 +0100

    [add] products

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,87 +2,35 @@
 
 @php
     $products = config('products');
-
 @endphp
 
 
 @section('content')
     <div class="container">
-        <div class="product">
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, recusandae perferendis ipsa cum
-                consequatur eos ipsum non unde reiciendis fuga asperiores magni ad sapiente quidem soluta doloremque cumque
-                sint quo. Officia tempore voluptatibus dolores saepe. Sed, eius? Aliquid nemo, doloribus earum dignissimos
-                iusto saepe minima labore! Quam reprehenderit minima perspiciatis assumenda accusantium reiciendis eum quas
-                dolore numquam enim rem quidem aspernatur sint vel dolores, corporis repudiandae mollitia eius doloribus
-                nulla explicabo ea sapiente? Consequuntur laborum dolorum vero quasi sapiente voluptatum a obcaecati esse
-                nihil in aliquam sint eius veniam quaerat cum, quos molestias accusamus placeat nostrum ea veritatis libero
-                perferendis. Natus odit necessitatibus, et dicta sint accusamus reiciendis fugiat ab nulla ipsa magni
-                voluptatum ad eligendi expedita aut ullam ratione voluptatibus placeat minus aliquid laudantium quia saepe.
-                Molestiae, nihil deleniti corrupti architecto consequuntur neque porro numquam sunt eum, incidunt nostrum
-                officia! Dicta rerum et porro quaerat qui deleniti, nesciunt mollitia eos optio iusto quis laborum
-                exercitationem nobis natus tenetur totam, quidem repellat consectetur possimus, enim voluptatum nihil.
-                Delectus blanditiis, accusantium similique amet dolore magni, at aliquam quas mollitia voluptate enim,
-                facere animi perspiciatis quos impedit numquam? Esse deserunt facilis at molestias, cumque aperiam, tempore
-                unde labore adipisci debitis, ipsum asperiores necessitatibus? Consequatur quibusdam suscipit voluptas
-                libero aut? Explicabo dolore officiis porro. Odit laborum, dolore a rem optio modi temporibus laudantium
-                unde consequuntur obcaecati illum eveniet similique tenetur quidem reprehenderit commodi, perferendis
-                accusamus nisi sint neque adipisci magnam nesciunt eos. Obcaecati odit autem dolor in temporibus laborum
-                sunt beatae iste expedita non, quas cupiditate perspiciatis necessitatibus veritatis deleniti est, enim
-                repellat. Dolores ipsa, at consequuntur, pariatur asperiores qui quia cumque ab corporis laudantium
-                assumenda accusamus dolore ducimus? Eum fugiat saepe perferendis debitis facere incidunt veritatis assumenda
-                veniam! Nihil omnis sapiente hic doloremque culpa consequuntur corrupti incidunt autem cumque officiis minus
-                voluptates, expedita quia illo. Sit nemo deleniti accusamus sequi omnis distinctio tenetur dolores delectus
-                consectetur incidunt expedita sed, officiis nobis fugit cum. Ullam incidunt, illum nemo ad odio similique
-                nesciunt, accusantium nulla ut voluptates iste harum ea accusamus doloremque asperiores corporis aperiam non
-                porro aut. Repudiandae officiis temporibus sunt accusantium nostrum harum animi, sit voluptatibus, laborum
-                deleniti dolorem perspiciatis laudantium iusto voluptatem distinctio sequi repellat ab exercitationem
-                tempore eos reiciendis dolorum eius! Reprehenderit dolorum ducimus, unde autem quas fuga! Aperiam, rerum
-                iusto tempora et magnam non accusantium quidem repellendus facere iure dolorum voluptatum ea nesciunt
-                obcaecati beatae ipsam similique deserunt quibusdam illo doloribus dolore est reprehenderit at quae! Est
-                dolor ipsam architecto voluptatibus! Veritatis facere blanditiis alias eos incidunt, inventore natus?
-                Dignissimos, earum est totam itaque hic, quisquam voluptate nemo repellat sequi neque dolorum deleniti
-                delectus repellendus libero soluta pariatur aperiam magnam officia nihil labore quos incidunt, enim rerum
-                officiis? Cupiditate quod quas impedit perspiciatis molestiae, unde reiciendis ducimus quibusdam nulla,
-                tempora laborum, fugit molestias magnam vitae eaque suscipit repellat pariatur? Accusantium, reprehenderit!
-                Illo aperiam at alias delectus, unde optio quae sit nisi non cupiditate? Porro, iusto corrupti voluptatem
-                maiores delectus soluta enim ipsa saepe, cumque ad ducimus numquam, repudiandae omnis velit doloremque
-                necessitatibus facere hic quod! Praesentium rerum quis totam deserunt maiores voluptas officia, voluptatem
-                rem quo est quaerat voluptate, libero ipsam aperiam doloremque pariatur labore consequatur sint eligendi
-                fugiat itaque molestias. Dolorem blanditiis, suscipit dolorum iusto nam atque dolore, unde delectus sunt
-                adipisci ad, aut quisquam! Omnis, dolor labore nihil deleniti est vitae voluptas obcaecati dolorum ab,
-                nostrum corporis ex placeat officia laboriosam quod nisi maxime. Eligendi officiis, esse natus ut nesciunt
-                corrupti qui ad in suscipit non ipsam nemo debitis sapiente recusandae quo earum id doloremque officia quae.
-                Mollitia corrupti quasi repellat necessitatibus at et ipsum nam commodi sit nobis debitis aperiam,
-                repellendus eius delectus temporibus doloribus, quisquam molestiae assumenda totam obcaecati omnis quidem?
-                Praesentium doloremque cum repellendus. Voluptatibus, ex. Repudiandae consequatur cum corporis aspernatur
-                architecto sit aliquid? Placeat mollitia, perferendis ipsa eligendi ipsam nobis laborum deleniti sunt amet
-                ullam voluptatum architecto similique natus eos error cupiditate quisquam accusamus accusantium sed
-                provident, dolorem fugit cumque doloribus. Ex ut eius quae modi magni omnis laboriosam sed provident tempora
-                earum quod optio saepe minima, nam odio repellat commodi assumenda illo, minus facere suscipit? Dolore nulla
-                impedit a beatae voluptate nostrum exercitationem, inventore distinctio modi atque quasi aspernatur velit
-                reiciendis dolorem numquam harum! Laboriosam veniam ad error vero, magnam consectetur, optio, blanditiis
-                magni assumenda odio cumque debitis corporis. Tempora reprehenderit quae sit nam nulla esse dolorem laborum
-                at! Nemo earum, autem cupiditate non reprehenderit ratione odio velit consequatur ad enim quaerat dolores
-                saepe, rerum distinctio in et. Modi assumenda, ea consequatur deserunt dolorum recusandae nemo incidunt
-                provident dolores eligendi voluptas officia neque rem doloremque. Odio necessitatibus, illum qui provident
-                facere sed nostrum optio dolorum, iusto beatae velit eligendi consequuntur amet fuga, atque odit veniam
-                molestiae consequatur? Placeat quos ab accusamus laborum. Facere ratione totam accusantium veritatis natus
-                alias neque dicta voluptas accusamus expedita a perspiciatis doloribus corrupti molestiae atque officia
-                tempore omnis consequatur, cum soluta illum laborum. Repellendus hic sequi unde obcaecati voluptatum? Atque
-                distinctio iste et, culpa vero sapiente autem perferendis iure fuga consectetur aliquid libero molestiae
-                facilis aspernatur vel, expedita voluptate, praesentium harum magnam aut eaque quibusdam veniam. Atque,
-                libero. Ullam, assumenda aperiam harum eligendi accusantium perferendis reprehenderit cumque. Praesentium
-                qui ipsam voluptas perspiciatis pariatur, nulla optio commodi, dolorum obcaecati nobis accusamus a
-                accusantium fugit. Quibusdam perspiciatis cum ex placeat quaerat, cumque fugiat alias nisi commodi maiores
-                debitis voluptatum nam? Sunt veritatis ipsa, non ullam dignissimos asperiores rerum eum velit! Assumenda
-                sint, similique, reiciendis consequuntur, ratione dolorum placeat et incidunt labore nam vel? Quibusdam
-                aperiam at doloribus quos ut sint natus qui omnis, veniam doloremque aliquam officia laborum rerum cumque
-                dolor corporis numquam unde eum consequatur voluptatum nihil aut vero eligendi? Accusantium placeat unde,
-                voluptate aliquam officiis ex temporibus saepe quaerat iste dolores quis voluptatem iure veniam praesentium?
-                Tenetur tempore iste libero dicta nisi deleniti sit odit voluptatum praesentium quaerat cumque non
-                voluptatem accusamus ipsum ratione distinctio magni consectetur delectus at, labore corporis eos! Saepe,
-                corporis voluptatibus ipsum porro pariatur debitis doloremque animi enim fugiat labore excepturi voluptate
-                laboriosam.</p>
-        </div>
+
+        @forelse ($products as $product)
+            <div class="product">
+                <div class="img-product">
+                    <img src="/img/{{ $product['frontImage'] }}" alt="" class="imgdefault">
+                    <img src="/img/{{ $product['backImage'] }}" alt="" class="imghover">
+                    <span class="{{ $product['isInFavorites'] ? 'active' : '' }} heart">&hearts;</span>
+                    <div class="target">
+                        @foreach ($product['badges'] as $badge)
+                            <span class="{{ $badge['type'] }}">{{ $badge['value'] }}</span>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="description">
+                    <span>{{ $product['brand'] }}</span>
+                    <h3><strong>{{ $product['name'] }}</strong></h3>
+                </div>
+                <div class="price">
+                    <span class="discounted">{{ $product['price'] }} &euro;</span>
+                    <span class="original">{{ $product['price'] }} &euro;</span>
+                </div>
+            </div>
+        @empty
+            <h1>Non ci sono prodotti</h1>
+        @endforelse
     </div>
 @endsection
